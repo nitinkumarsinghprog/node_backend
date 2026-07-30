7d2b03177583e9a152de81f4293b16aad851143d
Nitin Kumar <nitinsingh.kmr@gmail.com>
2026-07-30 19:11:16 +0530

    added middleware and upload funcanility

---
+++ src/utils/upload.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});    
+
+const uploadOnServer = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+        //upload file on the cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        //file has been uploaded 
+        console.log("file has been upload", response.url );
+        return response;
+
+    } catch (error) {
+        // remove the local tem save file 
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
